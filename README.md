42ed211794dec631fe7face34a31f5c106903d3c
Mark Wynn-Mackenzie <mwynnmackenzie@cloudbees.com>
2023-03-13 14:16:38 +0000

    Init a no-frills browser sample app

---
+++ README.md
@@ -0,0 +1,4 @@
+
+1.) Put your environment key into index.js (in the Rox.setup() call).
+1.) `npm install`
+1.) `npm run build`
---
+++ index.js
@@ -0,0 +1,20 @@
+const Rox = require('rox-browser');
+
+const flags = {
+  enableTutorial: new Rox.Flag(),
+  titleColors: new Rox.RoxString('White', ['White', 'Blue', 'Green', 'Yellow']),
+  titleSize: new Rox.RoxNumber(12, [12, 14, 18, 24])
+};
+
+async function initFeatureManagement() {
+	Rox.register(flags);
+	console.log('Calling setup');
+	await Rox.setup('612f5d0a30c0b442ab612c3c');
+	console.log('Done setup');
+	console.log('flags.enableTutorial.isEnabled()=', flags.enableTutorial.isEnabled());
+	console.log('flags.titleColors.getValue()=', flags.titleColors.getValue());
+}
+
+console.log('Starting app!!!');
+initFeatureManagement();
+console.log('Finished app!!!');
