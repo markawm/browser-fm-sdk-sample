8ad37e90b844af306f095d7135b61479e40464ff
Mark Wynn-Mackenzie <mwynnmackenzie@cloudbees.com>
2023-05-12 15:38:22 +0100

    Try it out with the showCard flag.

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 const Rox = require('rox-browser');
 
 const flags = {
-	defaultTest: new Rox.RoxString('from-code', ['value-1', 'value-2', 'value-3'])
+	//defaultTest: new Rox.RoxString('from-code', ['value-1', 'value-2', 'value-3'])
+	showCard: new Rox.RoxString(
+		'{"justify": "left", "cssStyle": "", "showCard": "false", "showCardName": "true"}',
+		['{"justify": "left", "cssStyle": "", "showCard": "false", "showCardName": "true"}', '{"justify": "left", "cssStyle": "", "showCard": "true", "showCardName": "false"}'])
 };
 
 const configurationFetchedHandler = fetcherResults => {
   console.log('FETCHED CONFIG! result: ', fetcherResults);
 
-  console.log('New result is: ', flags.defaultTest.getValue());
-  console.log('DONE');
+	console.log('==============================================================================================================');
+  console.log('showCard after fetching: ', flags.showCard.getValue());
+  console.log('==============================================================================================================');
 };
 
 async function initFeatureManagement() {
 	Rox.register(flags);
-	Rox.setCustomBooleanProperty("my-stickiness", function(){
-   //return '1'; // => value-3
-		return '3'; // => value-1
+	Rox.setCustomBooleanProperty("syf-widget-token", function(){
+		return 'abcdefg'; // => resolves to the true/false variant.
+		//return '43534534'; // => resolves to the false/true variant.
 	});
-	console.log('defaultTest value @1: ', flags.defaultTest.getValue());
+	Rox.setCustomStringProperty("partnerId", function(){
+  	return 'PI53421676';	// Results in one of the values from targetting.
+  	//return 'zzz';				// Results in default value-from-code (because flag rule not matched).
+	});
+	console.log('showCard value @1: ', flags.showCard.getValue());
 	console.log('Calling setup');
-	await Rox.setup('612f5d0a30c0b442ab612c3c', {
+	const myEnvId = '612f5d0a30c0b442ab612c3c';		// Mine
+	// TODO: insert your key here:
+	const synchronyEnvId = '<TODO>';	// Synchrony
+	await Rox.setup(synchronyEnvId, {
 		configurationFetchedHandler,
 		//debugLevel: 'verbose',
-		//configuration: {disableNetworkFetch: true}
+		configuration: {disableNetworkFetch: true}
 	});
 	console.log('Done setup');
-	console.log('defaultTest value @2: ', flags.defaultTest.getValue());
+	console.log('showCard value @2: ', flags.showCard.getValue());
 
 	document.write('<p>');
-	document.write('Default test: ', flags.defaultTest.getValue());
+	document.write('showCard: ', flags.showCard.getValue());
 	document.write('</p>');
 }
 
