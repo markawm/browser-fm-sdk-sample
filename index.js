3aad6410fc5e03d5f85ccbbb4574f43ffd088570
Mark Wynn-Mackenzie <mwynnmackenzie@cloudbees.com>
2023-05-17 11:42:42 +0100

    Update index.js
    Even simplerer.

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const Rox = require('rox-browser');
 
 const flags = {
+  // The classic sample flags. Optionally, switch this flags to your own:
   enableTutorial: new Rox.Flag(),
   titleColors: new Rox.RoxString('White', ['White', 'Blue', 'Green', 'Yellow']),
-  titleSize: new Rox.RoxNumber(12, [12, 14, 18, 24])
 };
 
 async function initFeatureManagement() {
 	Rox.register(flags);
-	console.log('Calling setup');
+	// Insert your own environment key here:
 	await Rox.setup('612f5d0a30c0b442ab612c3c');
-	console.log('Done setup');
-	console.log('flags.enableTutorial.isEnabled()=', flags.enableTutorial.isEnabled());
-	console.log('flags.titleColors.getValue()=', flags.titleColors.getValue());
 
 	document.write('<p>')
 	document.write('flags.enableTutorial.isEnabled()=', flags.enableTutorial.isEnabled());
